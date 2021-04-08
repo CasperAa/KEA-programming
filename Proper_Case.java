998814f208c1d6386a56ccc1ec6bf6c66962d6ea
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2021-04-08 12:15:19 +0200

    Tilføjet kommentar  - Casper

--- Proper_Case.java
+++ Proper_Case.java
@@ -10,33 +10,39 @@ public class Proper_Case {
     • Ord på max.3 bogstaver skal skrives med småt.
      */
     public static void main(String[] args) {
+        //Using the Scanner to ask the user for a sentence
         Scanner userSentence = new Scanner(System.in);
         System.out.println("Write a sentence, i will change it!");
         String sentenceToChange = userSentence.nextLine();
+
+        //Calling the method with the user sentence
         System.out.println(sentenceChanger(sentenceToChange));
 
     }
 
-
+        //Method that changes the sentence
     public static String sentenceChanger(String sentence) {
+        //Arraylist to store the individual words in teh sentence
         ArrayList<String> sentenceDivider = new ArrayList<>();
-
-
+        //Splitting the sentence with space
         String[] sentenceDivided = sentence.split(" ");
 
-
+        //For loop for each word in the ArrayList
         for (String word : sentenceDivided) {
-
+            //Checks if the is upper case only
             if (word.equals(word.toUpperCase())) {
                 sentenceDivider.add(word);
+            //Checks if the word is longer then 3 letters
             } else if (word.length() > 3) {
                 String wordToChange = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
                 sentenceDivider.add(wordToChange);
+            //the remaining words (less then 3 letter words)
             } else {
                 String shortWord = word.toLowerCase();
                 sentenceDivider.add(shortWord);
             }
         }
+        //Combining the sentence with the updated words and returning it
         return String.join(" ",sentenceDivider);
     }
 }
